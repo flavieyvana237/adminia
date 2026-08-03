8c4297726143d123c4620ac50077ee8031486b2e
Flavie Meffo <flaviemeffo68@gmail.com>
2026-08-03 19:03:31 +0100

    configuration de Redoc

--- config/urls.py
+++ config/urls.py
@@ -6,6 +6,7 @@ from django.urls import path
 from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from drf_spectacular.views import SpectacularAPIView
+from drf_spectacular.views import SpectacularRedocView
 from drf_spectacular.views import SpectacularSwaggerView
 from rest_framework.authtoken.views import obtain_auth_token
 
@@ -39,6 +40,11 @@ urlpatterns += [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path(
+    "api/redoc/",
+    SpectacularRedocView.as_view(url_name="api-schema"),
+    name="redoc",
+),
 ]
 
 if settings.DEBUG:
