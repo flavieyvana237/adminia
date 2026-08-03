66fe1305f61d9729980c1a9e92630fca87119882
Flavie Meffo <flaviemeffo68@gmail.com>
2026-08-03 15:16:46 +0100

    feat: setup project base with local postgres configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -11,10 +11,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 APPS_DIR = BASE_DIR / "adminia"
 env = environ.Env()
 
-READ_DOT_ENV_FILE = env.bool("DJANGO_READ_DOT_ENV_FILE", default=False)
-if READ_DOT_ENV_FILE:
-    # OS environment variables take precedence over variables from .env
-    env.read_env(str(BASE_DIR / ".env"))
+# Forcer la lecture du fichier .env à la racine
+env.read_env(str(BASE_DIR / ".env"))
 
 # GENERAL
 # ------------------------------------------------------------------------------
